10acaad9fa304fb51ac0c60597b1762d050d7e6b
JoeJ1 <joeedwardharry1@gmail.com>
2017-06-25 13:46:23 +0100

    Added numbering system to areasdef

--- main/main.py
+++ main/main.py
@@ -30,7 +30,7 @@ def areasdef(areasamount,syslvl,intro = 0):
         a = random.randint(1,25) + 64
         b = random.randint(1,25) + 64
         c = random.randint(1,25) + 64
-        areaname = "\nNEUTRAL-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100))
+        areaname = "\n1. NEUTRAL-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100))
         areaname = ''.join(areaname)
         if(a2 == ""):
             b2 = "NEU"
@@ -45,21 +45,25 @@ def areasdef(areasamount,syslvl,intro = 0):
             a5 = areaname
             b5 = "NEU"
     elif(intro == 0):
+        n =0
         for i in range(0,areasamount):
             areatype = random.randint(1,10)
             if(areatype == 1):
+                n = n +1
                 a = random.randint(1,25) + 64
                 b = random.randint(1,25) + 64
                 c = random.randint(1,25) + 64
-                areaname = "\nUNKNOWN-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100))
+                areaname = "\n",str(n),". UNKNOWN-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100))
                 areaname = ''.join(areaname)
                 a1 = areaname
                 b1 = "UNK"
+                print(areaname)
             elif(areatype ==2 or areatype ==  3 or areatype == 4 or areatype == 5):
+                n = n+1
                 a = random.randint(1,25) + 64
                 b = random.randint(1,25) + 64
                 c = random.randint(1,25) + 64
-                areaname = "\nNEUTRAL-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100))
+                areaname = "\n",str(n),". NEUTRAL-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100))
                 areaname = ''.join(areaname)
                 if(a2 == ""):
                     b2 = "NEU"
@@ -73,11 +77,13 @@ def areasdef(areasamount,syslvl,intro = 0):
                 elif(a5 == ""):
                     a5 = areaname
                     b5 = "NEU"
+                print(areaname)
             elif(areatype == 6 or areatype == 7):
+                n = n+1
                 a = random.randint(1,25) + 64
                 b = random.randint(1,25) + 64
                 c = random.randint(1,25) + 64
-                areaname = "\nFRIENDLY-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100))
+                areaname = "\n",str(n),". FRIENDLY-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100))
                 areaname = ''.join(areaname)
                 if(a6 == ""):
                     b6 = "FRE"
@@ -85,11 +91,13 @@ def areasdef(areasamount,syslvl,intro = 0):
                 if(a7 == ""):
                     b7 = "FRE"
                     b6 = areaname
+                print(areaname)
             elif(areatype == 8 or areatype == 9):
+                n = n+1
                 a = random.randint(1,25) + 64
                 b = random.randint(1,25) + 64
                 c = random.randint(1,25) + 64
-                areaname = "\nHOSTILE-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100))
+                areaname = "\n",str(n),". HOSTILE-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100))
                 areaname = ''.join(areaname)
                 if(a8 == ""):
                     b8 = "HOS"
@@ -97,25 +105,27 @@ def areasdef(areasamount,syslvl,intro = 0):
                 elif(a9 == ""):
                     b9 = "HOS"
                     a9 = areaname
-        else:
-            a = random.randint(1,25) + 64
-            b = random.randint(1,25) + 64
-            c = random.randint(1,25) + 64
-            areaname = "\nSTATION-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100))
-            areaname = ''.join(areaname)
-            b10 = "STA"
-            a10 = areaname
+                print(areaname)
+            else:
+                n = n+1
+                a = random.randint(1,25) + 64
+                b = random.randint(1,25) + 64
+                c = random.randint(1,25) + 64
+                areaname = "\n",str(n),". STATION-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100))
+                areaname = ''.join(areaname)
+                b10 = "STA"
+                a10 = areaname
+                print(areaname)
 
 def scan(syslvl,scansize,intro = 0): #intro should be whether or not this is the scan in the intro (1 if it is 0 if it's not)
     areasamount = random.randint(1,(scansize*syslvl))
     areas = areasdef(areasamount,syslvl,intro)
-
-    print(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10)
+    #print(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10)
 def intro():
     print("You are a ruthless mercenary, a soldier for hire (space pirate).\nYour mission: to reach the centre of the galaxy and discover what lies there.\nBut for now, think a little smaller, outside your window in the distance you see a small ship, some quick scans tell you there might be weapons aboard.\nBoard it kill any hostiles and leave with the weapons")
     input("Press enter to continue")
     clear(100)
-    scan(1,1,1)
+    scan(1,10)
 
 
 if(input("Do you wish to skip intro/tuorial? (y/n)") == "n"):
