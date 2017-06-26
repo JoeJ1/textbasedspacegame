226e74fc3c0baa38b50de77e3368328c9383295b
JoeJ1 <joeedwardharry1@gmail.com>
2017-06-26 16:37:05 +0100

    Fixed help bug and area name bug

--- main/main.py
+++ main/main.py
@@ -26,14 +26,16 @@ scanned = 0
 def console():
     clear(100)
     print("""
-                  _____  _____    _____  _    _    __      _____  _  _   ___  
-         /\\      / ____||_   _|  / ____|| |  | |   \\ \\    / /__ \\| || | / _ \\ 
+                  _____  _____    _____  _    _    __      _____  _  _   ___
+         /\\      / ____||_   _|  / ____|| |  | |   \\ \\    / /__ \\| || | / _ \\
         /  \\    | (___    | |   | (___  | |__| |    \\ \\  / /   ) | || || (_) |
        / /\\ \\    \\___ \\   | |    \\___ \\ |  __  |     \\ \\/ /   / /|__   _\__, |
-      / ____ \\ _ ____) | _| |_ _ ____) || |  | |_     \\  /   / /_ _ | |   / / 
-     /_/    \\_(_)_____(_)_____(_)_____(_)_|  |_(_)     \\/   |____(_)|_|  /_/  
-                                                                          
-                                                                         
+      / ____ \\ _ ____) | _| |_ _ ____) || |  | |_     \\  /   / /_ _ | |   / /
+     /_/    \\_(_)_____(_)_____(_)_____(_)_|  |_(_)     \\/   |____(_)|_|  /_/
+
+     Advanced     Ship  Interactive    Shell
+
+
     """)
     choice = input(": ")
     if(choice == "scan"):
@@ -50,26 +52,13 @@ def areasdef(areasamount,syslvl,intro = 0): #areasdef defines the type of area i
         a = random.randint(1,25) + 64 #a,b and c are ascii values of randomised letters for the names of objects
         b = random.randint(1,25) + 64
         c = random.randint(1,25) + 64
-        areaname = "1. NEUTRAL-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100)) #creating the name of the object/area
+        areaname = "NEUTRAL-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100)) #creating the name of the object/area
         areaname = ''.join(areaname) #joining the list of values into one string (the name)
-        if(a2 == ""): #checking to see whether or not to override the variables
-            b2 = "NEU"
-            a2 = areaname
-            areas[n] = str(a2)#adding to areas array with this area
-        elif(a3 == ""):
-            a3 = areaname
-            b3 = "NEU"
-            areas[n] = str(a3)#adding to areas array with this area
-        elif(a4 == ""):
-            a4 = areaname
-            b4 = "NEU"
-            areas[n] = str(a4)#adding to areas array with this area
-        elif(a5 == ""):
-            a5 = areaname
-            b5 = "NEU"
-            areas[n] = str(a5)#adding to areas array with this area
         print("\t\t| \/value  \/full name\t|")
         print("\t\t|",areaname,"\t|")
+        a1 = areaname
+        areas[n] = a1
+        b1 = "NEU"
     elif(intro == 0):
         n =0
         for i in range(0,areasamount):
@@ -79,72 +68,80 @@ def areasdef(areasamount,syslvl,intro = 0): #areasdef defines the type of area i
                 a = random.randint(1,25) + 64 #a,b and c are ascii values of randomised letters for the names of objects
                 b = random.randint(1,25) + 64
                 c = random.randint(1,25) + 64
-                areaname = str(n),". UNKNOWN-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100)) #creating the name of the object/area
+                areaname = "UNKNOWN-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100)) #creating the name of the object/area
                 areaname = ''.join(areaname) #joining the list of values into one string (the name)
                 a1 = areaname
                 b1 = "UNK"
                 areas[n] = a1
-                print("\t\t|",areaname,"\t|")
+                print("\t\t| ",n,". ",a1,"\t|")
             elif(areatype ==2 or areatype ==  3 or areatype == 4 or areatype == 5): #Making it a 40% chance to see a neutral ship
                 n = n+1 # n defines what number to apply to each area
                 a = random.randint(1,25) + 64 #a,b and c are ascii values of randomised letters for the names of objects
                 b = random.randint(1,25) + 64
                 c = random.randint(1,25) + 64
-                areaname = str(n),". NEUTRAL-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100))  #creating the name of the object/area
+                areaname = "NEUTRAL-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100))  #creating the name of the object/area
                 areaname = ''.join(areaname) #joining the list of values into one string (the name)
                 if(a2 == ""): #checking to see whether or not to override the variables
                     b2 = "NEU"
                     a2 = areaname
-                    areas[n] = a1
+                    areas[n] = a2
                 elif(a3 == ""):
                     a3 = areaname
                     b3 = "NEU"
-                    areas[n] = a1
+                    areas[n] = a3
                 elif(a4 == ""):
                     a4 = areaname
                     b4 = "NEU"
+                    areas[n] = a4
                 elif(a5 == ""):
                     a5 = areaname
                     b5 = "NEU"
-                print("\t\t|",areaname,"\t|")
+                    areas[n] = a5
+                print("\t\t| ",n,". ",areaname,"\t|")
             elif(areatype == 6 or areatype == 7): #20% chance of a friendly ship being generated
                 n = n+1 # n defines what number to apply to each area
                 a = random.randint(1,25) + 64 #a,b and c are ascii values of randomised letters for the names of objects
                 b = random.randint(1,25) + 64
                 c = random.randint(1,25) + 64
-                areaname = str(n),". FRIENDLY-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100)) #creating the name of the object/area
+                areaname = "FRIENDLY-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100)) #creating the name of the object/area
                 areaname = ''.join(areaname) #joining the list of values into one string (the name)
                 if(a6 == ""): #checking to see whether or not to override the variables
                     b6 = "FRE"
                     a6 = areaname
+                    areas[n] = a6
                 if(a7 == ""):
                     b7 = "FRE"
                     b6 = areaname
-                print("\t\t|",areaname,"\t|")
+                    areas[n] = a7
+                print("\t\t| ",n,". ",areaname,"\t|")
             elif(areatype == 8 or areatype == 9): #20% chance of creating a hostile ship
                 n = n+1 # n defines what number to apply to each area
                 a = random.randint(1,25) + 64 #a,b and c are ascii values of randomised letters for the names of objects
                 b = random.randint(1,25) + 64
                 c = random.randint(1,25) + 64
-                areaname = str(n),". HOSTILE-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100)) #creating the name of the object/area
+                areaname = "HOSTILE-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100)) #creating the name of the object/area
                 areaname = ''.join(areaname) #joining the list of values into one string (the name)
                 if(a8 == ""): #checking to see whether or not to override the variables
                     b8 = "HOS"
                     a8 = areaname
+                    areas[n] = a8
                 elif(a9 == ""):
                     b9 = "HOS"
                     a9 = areaname
-                print("\t\t|",areaname,"\t|")
+                    areas[n] = a9
+                print("\t\t| ",n,". ",areaname,"\t|")
             else: # 10% chance of generating a station
                 n = n+1 # n defines what number to apply to each area
                 a = random.randint(1,25) + 64 #a,b and c are ascii values of randomised letters for the names of objects
                 b = random.randint(1,25) + 64
                 c = random.randint(1,25) + 64
-                areaname = str(n),". STATION-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100)) #creating the name of the object/area
+                areaname = "STATION-",chr(a),chr(b),chr(c),"-",str(random.randint(0,100)) #creating the name of the object/area
                 areaname = ''.join(areaname) #joining the list of values into one string (the name)
                 b10 = "STA"
                 a10 = areaname
-                print("\t\t|",areaname,"\t|")
+                areas[n]=a10
+                print("\t\t| ",n,". ",areaname,"\t|")
+        print(areas)
 
 def scan(syslvl,scansize,intro = 0): #intro should be whether or not this is the scan in the intro (1 if it is 0 if it's not)
     global n, areas, scanned
@@ -160,19 +157,38 @@ def scan(syslvl,scansize,intro = 0): #intro should be whether or not this is the
     time.sleep(0.5)
     areasamount = random.randint(1,(scansize*syslvl))
     print("\t\t _______________________") #the box around the objects displayed in the scanner
-    if(scanned == 0): 
+    if(scanned == 0):
         areasdef(areasamount,syslvl,intro)
         scanned = 1
     else:
         i = 0
         while(i<n):
-            print(areas[i])
+            print(str(areas[i]))
             i= i+1
 
     print("\t\t|_______________________|") # the bottom half of the box around objects in the scanner
     #print(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10)
     print("\nType \"h\" to show help.")
     choice = input(": ") # taking input from player
+    if(choice == "h" or choice == "help" or choice == "H" or choice == "Help" or choice == "HELP"):
+        print("\n\nDo you wish to:\n\t1. scan a chosen object in more detail, for life signs, weaponry etc.\n\t2. Fly to the chosen object and prepare to dock/board/land.\n\t3. Exit Scanner")
+        choice = input(":")
+        if(choice == "scan" or choice == "1" or choice == "one" or choice == "One" or choice == "Scan" or choice == "SCAN" or choice == "s" or choice == "S" or choice == "1k."): #processing input
+            print("Which object would you like to scan in detail? (full name or value)")
+            choice = input(": ")
+            if(choice.isdigit() and choice<n):
+                print("choice correct") #for debugging purposes
+                #stuff here
+            elif(str(choice) in areas):
+                print("choice correct") #for debugging purposes
+            else:
+                print("that wasn\'t one of the options")
+                #stuff here
+        elif(choice == "2" or choice == "two" or choice == "Two" or choice == "Fly" or choice == "fly" or choice == "dock" or choice == "board"):
+            print("Which object would you like to fly to? (full name or value)")
+            choice = input(": ")
+            #stuff here
+        scan(syslvl,scansize,intro)
     if(choice == "scan" or choice == "1" or choice == "one" or choice == "One" or choice == "Scan" or choice == "SCAN" or choice == "s" or choice == "S" or choice == "1k."): #processing input
         print("Which object would you like to scan in detail? (full name or value)")
         choice = input(": ")
@@ -188,10 +204,7 @@ def scan(syslvl,scansize,intro = 0): #intro should be whether or not this is the
         print("Which object would you like to fly to? (full name or value)")
         choice = input(": ")
         #stuff here
-    elif(choice == "h"):
-        print("\n\nDo you wish to:\n\t1. scan a chosen object in more detail, for life signs, weaponry etc.\n\t2. Fly to the chosen object and prepare to dock/board/land.\n\t3. Exit Scanner")
-        scan(syslvl,scansize,intro)
-        
+
 def intro():
     clear(100)
     print("You are a ruthless mercenary, a soldier for hire (space pirate).\nYour mission: to reach the centre of the galaxy and discover what lies there.\nBut for now, think a little smaller, outside your window in the distance you see a small ship, some quick scans tell you there might be weapons aboard.\nBoard it kill any hostiles and leave with the weapons") #flavour text
