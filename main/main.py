0f53867cbf5d67b3a0982edae6d191109ef3bca0
M4G13 <msandreereid@gmail.com>
2017-06-26 21:15:01 +0100

    Update main.py

--- main/main.py
+++ main/main.py
@@ -41,7 +41,7 @@ def console():
     print("type \"help\" or \"h\"")
     choice = input(": ")
     if(choice.lower() == "scan" or choice.lower() == "s" ):
-        scan()
+        scan.scan()
     if(choice.lower() == "h" or choice.lower() == "help"):
         print("All commands are written in lower case.")
         clear(2)
@@ -57,47 +57,6 @@ def console():
 
 def clear(amount): #defining clear, a funcion which prints a certain amount of empty lines
     print("\n"*amount)
-
-def marauderer(syslvl,scanned,intro = 0):
-    global areas
-    clear(100)
-    current = ""
-    ships = ["","","","","","","","","","","","","","","","","","",""]
-    a = 1
-    print("""
-               __  ___                           __                           ___    ____
-              /  |/  /___ _ ____ ___ _ __ __ ___/ /___  ____ ___  ____  _  __<  /   |_  /
-             / /|_/ // _ `// __// _ `// // // _  // -_)/ __// -_)/ __/ | |/ // /_  _/_ <
-            /_/  /_/ \\_,_//_/   \\_,_/ \\_,_/ \\_,_/ \\__//_/   \\__//_/    |___//_/(_)/____/
-
-
-    """)
-    if(scanned == 0):
-        print("Must scan for ships before running.")
-    else:
-        print("Which ship (no stations or unknown objects) do you wish to scan? (full name or value)")
-        print("\t\t _______________________")
-        while(a<=n):
-            current = str(areas[a])
-            if(current[:7] == "NEUTRAL" or current[:7] == "CORDIAL" or current[:7] == "HOSTILE"):
-                ships[a] = areas[a]
-                print("\t\t| ",a,str(ships[a]),"\t|")
-            a = a +1
-        if(intro == 1):
-            clear(1)
-            print("type the name of the ship")
-        print("\t\t|_______________________|")
-        choice = input(": ")
-        if(str(choice) in ships):
-            print("Worked!")
-            #stuff here
-        elif(choice.isdigit()):
-            if(int(choice) < a):
-                print("Worked!")
-                print(str(ships[int(choice)]))
-                #stuff here
-
-
 
 def areasdef(areasamount,syslvl,intro = 0): #areasdef defines the type of area in a system and its name
     global a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,n
