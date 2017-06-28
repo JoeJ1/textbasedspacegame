caf374823da2f5e875148df4970b33418902a03a
M4G13 <msandreereid@gmail.com>
2017-06-28 12:36:43 +0000

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,60 +1,7 @@
 import time
 import scan
 import intro
-
-def console(logo):
-    global shellprompt, su
-    if(logo == 1):
-#        clear(100)
-#        print("""
-#                  _____  _____    _____  _    _    __      _____  _  _   ___
-#         /\\      / ____||_   _|  / ____|| |  | |   \\ \\    / /__ \\| || | / _ \\
-#        /  \\    | (___    | |   | (___  | |__| |    \\ \\  / /   ) | || || (_) |
-#       / /\\ \\    \\___ \\   | |    \\___ \\ |  __  |     \\ \\/ /   / /|__   _\__, |
-#      / ____ \\ _ ____) | _| |_ _ ____) || |  | |_     \\  /   / /_ _ | |   / /
-#     /_/    \\_(_)_____(_)_____(_)_____(_)_|  |_(_)     \\/   |____(_)|_|  /_/
-#
-#     Advanced     Ship  Interactive    Shell          Version      2.49
-#
-#
-#
-#    """)
-        print("Type \"help\" to view a list of commands")
-        time.sleep(0.5)
-        shellprompt = "[",name.lower(),"@",shipname.lower()," ~]$ "
-        shellprompt = ''.join(shellprompt)
-        su = 0
-    choice = input(shellprompt)
-    if(choice.lower() == "scan"):
-        scan.scan(1,1,1)
-    elif(choice == "help"):
-        print("Advanced Ship Interactive Shell Version 2.49")
-        print("Commands available:")
-        print("scan: scans the area around your ship.")
-        print("clear: clear the console")
-    elif(choice == "su" and su == 0):
-        choice = input("Password: ")
-        if(choice == "doggos"):
-            shellprompt = "[",name.lower(),"@",shipname.lower()," ~]# "
-            shellprompt = ''.join(shellprompt)
-            su = 1
-        else:
-            print("na")
-    elif(choice == "clear"):
-        clear(50)
-        time.sleep(0.1)
-        clear(50)
-    elif(choice == "exit" and su == 1):
-        shellprompt = "[",name.lower(),"@",shipname.lower()," ~]$ "
-        shellprompt = ''.join(shellprompt)
-        su = 0
-        clear(50)
-        time.sleep(0.1)
-        clear(50)
-    else:
-        print("asish:",choice,": command not found")
-    console(0)
-
+import console
 
 def clear(amount): #defining clear, a funcion which prints a certain amount of empty lines
     print("\n"*amount)
@@ -70,4 +17,4 @@ if(input("Do you wish to skip intro/tutorial? (y/n)") == "n"): #deciding whether
     intro #calling intro mission
 else:
     clear(100)
-    console(1)
+    console.console(1)
