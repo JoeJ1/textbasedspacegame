07882d0cb9b004f146c83afd188c092d4c4ef7fe
M4G13 <msandreereid@gmail.com>
2017-06-28 09:45:46 +0100

    Update scan.py

--- main/scan.py
+++ main/scan.py
@@ -169,3 +169,35 @@ def areasdef(areasamount,syslvl,intro = 0): #areasdef defines the type of area i
                 areas[n]=a10
                 print("\t\t| ",n,". ",areaname,"\t|")
         #print(areas)
+
+def dscan(syslvl,scanned,intro = 0):
+    global areas
+    clear(100)
+    current = ""
+    ships = ["","","","","","","","","","","","","","","","","","",""]
+    a = 1
+    if(scanned == 0):
+        print("Must scan for ships before running.")
+    else:
+        print("Which ship (no stations or unknown objects) do you wish to scan? (full name or value)")
+        print("\t\t _______________________")
+        while(a<=n):
+            current = str(areas[a])
+            if(current[:7] == "NEUTRAL" or current[:7] == "CORDIAL" or current[:7] == "HOSTILE"):
+                ships[a] = areas[a]
+                print("\t\t| ",a,str(ships[a]),"\t|")
+            a = a +1
+        if(intro == 1):
+            clear(1)
+            print("type the name of the ship")
+        print("\t\t|_______________________|")
+        choice = input(": ")
+        if(str(choice) in ships):
+            print("Worked!")
+            #stuff here
+        elif(choice.isdigit()):
+            if(int(choice) < a):
+                print("Worked!")
+                print(str(ships[int(choice)]))
+                #stuff here
+
