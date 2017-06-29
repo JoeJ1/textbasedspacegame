a3f3d2f73a117f872f27b32bea31a5c4dde45e23
JoeJ1 <joeedwardharry1@gmail.com>
2017-06-29 16:47:18 +0100

    Update shipgen.py

--- main/shipgen.py
+++ main/shipgen.py
@@ -12,3 +12,18 @@ def shipgen(sizex,sizey):
     string = string.replace(",","")
     string = string.replace(" ","")
     string = [string[a:a+sizey] for a in range(0, len(string), sizey)]
+    return(string)
+def ship(sizex,sizey):
+    ship = shipgen(sizex,sizey)
+    elementlen = len(ship[1])
+    x = 1
+    y = 1
+    area = room(x,y,ship)
+    print(area)
+def room(x,y,ship):
+    if(x<len(ship[1])and y<len(ship)/x):
+        ypos = ship[y]
+        ypos = list(ypos)
+        pos = ypos[x]
+        return(pos)
+ship(8,4)
