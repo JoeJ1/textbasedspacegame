c51cd56a3c4b728c398092be65e697feb4566f3f
M4G13 <msandreereid@gmail.com>
2017-06-29 16:59:25 +0100

    yay

--- main/main.py
+++ main/main.py
@@ -9,6 +9,7 @@ def clear(amount): #defining clear, a funcion which prints a certain amount of e
 
 # And so it begins
 print("Best viewed in fullscreen.")
+notes
 var.var()
 time.sleep(1)
 clear(100)
