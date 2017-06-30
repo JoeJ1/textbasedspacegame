c117320cf20f4a1162abff753b9eb22e8cdb9186
M4G13 <msandreereid@gmail.com>
2017-06-30 17:20:14 +0100

    Update board.py

--- main/board.py
+++ main/board.py
@@ -1,16 +1,19 @@
 import var
-import levelgen
+import lvlgen
+import random
+
+
 def shipboard():
     option1 = ""
     option2 = ""
     option3 = ""
     option4 = ""
     if(var.area[7] == "NEUTRAL"):
-        level = shipgen(randint(5,9),randint(6,8))
+        level = lvlgen.shipgen(random.randint(5, 9), random.randint(6, 8))
     else:
-        level = shipgen(randint(3,7),randint(4,6))
-    options = optionscheck(0,0,level)
-    if(options[0] == True):
+        level = lvlgen.shipgen(random.randint(3, 7), random.randint(4, 6))
+    options = optionscheck(0, 0, level)
+    if(options[0] is True):
         if(option1 == ""):
             option1 = "right"
         elif(option2 == ""):
@@ -19,7 +22,7 @@ def shipboard():
             option3 = "right"
         else:
             option4 = "right"
-    if(options[1] = True):
+    if(options[1] is True):
         if(option1 == ""):
             option1 = "left"
         elif(option2 == ""):
@@ -28,7 +31,7 @@ def shipboard():
             option3 = "left"
         elif(option4 == ""):
             option4 = "left"
-    if(options[2] = True):
+    if(options[2] is True):
         if(option1 == ""):
             option1 = "up"
         elif(option2 == ""):
@@ -37,7 +40,7 @@ def shipboard():
             option3 = "up"
         elif(option4 == ""):
             option4 = "up"
-    if(options[3] = True):
+    if(options[3] is True):
         if(option1 == ""):
             option1 = "down"
         elif(option2 == ""):
@@ -59,31 +62,32 @@ def shipboard():
     if(option4 != ""):
         d = "\n"
     print("Options for movement are: ")
-    print(a,option1,b,option2,c,option3,d,option4)
+    print(a, option1, b, option2, c, option3, d, option4)
     print("\n")
     choice = input(": ")
     if(choice == option1 or choice == option2 or choice == option3 or choice == option4):
         if(choice.lower() == "right"):
-            x = x +1
+            var.x = var.x + 1
         elif(choice.lower() == "left"):
-            x = x -1
+            var.x = var.x - 1
         elif(choice.lower() == "up"):
-            y = y -1
+            var.y = var.y - 1
         elif(choice.lower() == "down"):
-            y = y +1
+            var.y = var.y + 1
 
-def optionscheck(x,y,level):
+
+def optionscheck(x, y, level):
     rightoption = False
     leftoption = False
     upoption = False
     downoption = False
-    if(levelgen.room(x+1,y,level) == 1 or levelgen.room(x+1,y,level) == 2):
+    if(lvlgen.room(x+1, y, level) == 1 or lvlgen.room(x+1, y, level) == 2):
         rightoption = True
-    if(levelgen.room(x-1,y,level) == 1 or levelgen.room(x-1,y,level) == 2):
+    if(lvlgen.room(x-1, y, level) == 1 or lvlgen.room(x-1, y, level) == 2):
         leftoption = True
-    if(levelgen.room(x,y+1,level) == 1 or levelgen.room(x,y+1,level) == 2):
+    if(lvlgen.room(x, y+1, level) == 1 or lvlgen.room(x, y+1, level) == 2):
         downoption = True
-    if(levelgen.room(x,y-1,level) == 1 or levelgen.room(x,y-1,level) == 2):
+    if(lvlgen.room(x, y-1, level) == 1 or lvlgen.room(x, y-1, level) == 2):
         upoption = True
-    options = [rightoption,leftoption,upoption,downoption]
+    options = [rightoption, leftoption, upoption, downoption]
     return(options)
