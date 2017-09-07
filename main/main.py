adeb3e99561d1de5ea573d1344e6388aca8419e1
JoeJ1 <joeedwardharry1@gmail.com>
2017-09-07 17:00:46 +0100

    Removed intro prompt as intro no longer exists

--- main/main.py
+++ main/main.py
@@ -10,8 +10,6 @@ time.sleep(1)  # waiting for one second for the user to read the previous messag
 var.clear(100)  # clearing the console.
 var.name = input("What is your name, mercenary? ")  # setting the name variable to whatever the user wants.
 var.shipname = input("What is the name of your trusty ship? ")  # setting the shipname variable to whatever the user wants.
-if(input("Do you wish to skip intro/tutorial? (y/n)") == "n"):  # calling intro if the player wants to play the intro.
-    intro.intro()  # ""
 else:  # otherwise, booting straight into the game.
     var.clear(100)  # ""
     console.console(1)  # ""
